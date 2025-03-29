17b9f63ed3d0bee9ca57238bbbbb708bdbc18e7d
praveentricker <praveenkumar800948@gmail.com>
2025-03-29 12:59:52 +0530

    Update 0_ARIF-BOT-1.js

--- models/commands/0_ARIF-BOT-1.js
+++ models/commands/0_ARIF-BOT-1.js
@@ -67,7 +67,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
    };
 
    if ((event.body.toLowerCase() == "owner") || (event.body.toLowerCase() == "Owner")) {
-     return api.sendMessage("༻𝐎𝐖𝐍𝐄𝐑:- ☞ARIF-BABU☜ ༺ ༒𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 〠 𝓟𝓾𝓻𝓿 〠.༒ ༒𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝༒:- ☞https://www.facebook.com/profile.php?id=61553634015672&mibextid=kFxxJD☜", threadID);
+     return api.sendMessage("༻𝐎𝐖𝐍𝐄𝐑:- ☞ABHI BABU☜ ༺ ༒𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 〠 𝓟𝓾𝓻𝓿 〠.༒ ༒𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝༒:- ☞https://www.facebook.com/profile.php?id=61555214872841☜", threadID);
    };
 
    if ((event.body.toLowerCase() == "tumhe banaya kon hai") || (event.body.toLowerCase() == "tumko banaya kisne")) {
@@ -247,19 +247,4 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
 
   if (event.body.includes("Bot") == 1 ||
    (event.body.includes("bot") == 1 ||
-   (event.body.includes("BOT") == 1 ||
-   (event.body.includes("BABU") == 1 ||
-   (event.body.includes("Babu") == 1 ||
-   (event.body.includes("babu") == 1 ||
-   (event.body.includes("boot") == 1 ||
-   (event.body.includes("Oye") == 1 ||
-   (event.body.includes("oye") == 1 ))))))))) {
-    var msg = {
-      body: `${rand}`
-    }
-    return api.sendMessage(msg, threadID, messageID);
-  };
-
-}
-
-module.exports.run = function({ api, event, client, __GLOBAL }) { }
+ 
